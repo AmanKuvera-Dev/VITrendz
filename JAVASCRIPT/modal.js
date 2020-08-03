cf5a453b0eebb7aca0644190a460b227e8f6b920
AmanKuvera-Dev <65402367+AmanKuvera-Dev@users.noreply.github.com>
2020-08-03 10:24:29 +0530

    Read More Buttons
    The read more buttons for all the Services have been added with listeners. Content to be added

--- JAVASCRIPT/modal.js
+++ JAVASCRIPT/modal.js
@@ -1,6 +1,48 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
+
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+
+const faculty = document.getElementById('facultyOpen')
+const question = document.getElementById('qbOpen')
+const resell = document.getElementById('resellOpen')
+const lab = document.getElementById('labOpen')
+const qb2 = document.getElementById('qb2Open')
+const cab = document.getElementById('cabOpen')
+const bod = document.getElementById('modBody')
+const modTitle = document.getElementById('modTitle')
+
+faculty.onclick = ()=>
+    {
+        bod.innerHTML="This is for Faculty"
+        modTitle = "Faculty Rating"
+    }
+
+question.onclick = ()=>{
+        bod.innerHTML="This is for Question Bank"
+        modTitle = "Question Bank"
+    }
+resell.onclick = ()=>
+    {
+        bod.innerHTML="This is for Buy and Resell"
+        modTitle = "Buy and Resell"
+    }
+lab.onclick = ()=>
+    {
+        bod.innerHTML="This is for Lab"
+        modTitle = "Lab Records"
+    }
+qb2.onclick = ()=>
+    {
+        bod.innerHTML="This is for extra question bank"
+        modTitle = "Question Bank"
+    }
+cab.onclick = ()=>
+    {
+        bod.innerHTML="This is for Cab"
+        modTitle = "Share A Cab"
+    }
+
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
